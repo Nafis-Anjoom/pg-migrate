765a6d3f245ead6cd2c77f4fdb5d08e26a38fad6
Nafis Anjoom <nafisanjoom@gmail.com>
2024-10-10 18:42:57 -0400

    created barebone init command

--- main.go
+++ main.go
@@ -1,48 +1,83 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
 	"pg-migrate/internal"
 )
 
+type Config struct {
+	CurrentVersion int    `json:"current_version"`
+	ConnURL        string `json:"conn_url"`
+}
+
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("invalid command")
+	if len(os.Args) < 2 {
+		fmt.Println("invalid command")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "migrate":
+		handleMigrate(os.Args[2:])
+	case "init":
+		handleInit(os.Args[2:])
+	default:
+		fmt.Println("invalid command")
+		os.Exit(1)
+	}
+}
+
+func handleInit(args []string) {
+	fs := flag.NewFlagSet("init", flag.ExitOnError)
+	sourcePtr := fs.String("source", "./", "source directory")
+	fs.Parse(args)
+
+    migrationsDirectory := *sourcePtr + "/migrations"
+
+	err := os.Mkdir(migrationsDirectory, 0750)
+	if err != nil && !os.IsExist(err) {
+        fmt.Println("error creating migration directory:", err)
+        os.Exit(1)
+	}
+
+    var config Config
+    json, err := json.MarshalIndent(config, "", "\t")
+    if err != nil {
+        fmt.Println("error encoding migration config to json:", err)
         os.Exit(1)
     }
 
-    switch os.Args[1] {
-    case "migrate":
-        handleMigrate(os.Args[2:])
-    default:
-        fmt.Println("invalid command")
+    err = os.WriteFile(migrationsDirectory + "/migrate.config", json, 0660)
+    if err != nil {
+        fmt.Println("error creating migration config file:", err)
         os.Exit(1)
     }
 }
 
 func handleMigrate(args []string) {
-    fs := flag.NewFlagSet("migrate", flag.ExitOnError)
-    sourcePtr := fs.String("source", "./", "source directory")
-    databasePtr := fs.String("database", "", "database connection url")
-    fs.Parse(args)
+	fs := flag.NewFlagSet("migrate", flag.ExitOnError)
+	sourcePtr := fs.String("source", "./", "source directory")
+	databasePtr := fs.String("database", "", "database connection url")
+	fs.Parse(args)
 
-    if *sourcePtr == "" {
-        fmt.Println("Invalid source directory:", *sourcePtr)
-        return
-    }
+	if *sourcePtr == "" {
+		fmt.Println("Invalid source directory:", *sourcePtr)
+		return
+	}
 
-    if *databasePtr == "" {
-        fmt.Println("Invalid database connection url:", *databasePtr)
-        return
-    }
+	if *databasePtr == "" {
+		fmt.Println("Invalid database connection url:", *databasePtr)
+		return
+	}
 
-    migrater, err := internal.NewMigrater(*sourcePtr, *databasePtr)
-    if err != nil {
-        fmt.Println("error creating migrater")
-        return
-    }
+	migrater, err := internal.NewMigrater(*sourcePtr, *databasePtr)
+	if err != nil {
+		fmt.Println("error creating migrater")
+		return
+	}
 
-    migrater.RunMigrations(internal.UP)
+	migrater.RunMigrations(internal.UP)
 }
