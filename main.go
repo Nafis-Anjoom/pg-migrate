27e0c3dc2a34720d4689ec93bb1dc2f60cfb8831
Nafis Anjoom <nafisanjoom@gmail.com>
2024-10-10 01:31:13 -0400

    added cli command and arguments for migrate

--- main.go
+++ main.go
@@ -1,26 +1,40 @@
 package main
 
 import (
-	// "flag"
+	"flag"
+	"fmt"
 	"os"
 	"pg-migrate/internal"
 )
 
 func main() {
-    // sourcePtr := flag.String("source", "", "source directory")
-    // databasePtr := flag.String("database", "", "database connection url")
-    //
-    // flag.Parse()
-
-    // migrater := newMigrater(*sourcePtr, *databasePtr)
-
-    source := "./migrations"
-    database := "postgres://postgres:admin@localhost:5431/dummy"
-    
-
-    _, err := internal.NewMigrater(source, database)
-    if err != nil {
+    if len(os.Args) < 2 {
+        fmt.Println("invalid command")
         os.Exit(1)
     }
 
+    switch os.Args[1] {
+    case "migrate":
+        handleMigrate(os.Args[2:])
+    default:
+        fmt.Println("invalid command")
+        os.Exit(1)
+    }
+}
+
+func handleMigrate(args []string) {
+    fs := flag.NewFlagSet("migrate", flag.ExitOnError)
+    sourcePtr := fs.String("source", "./", "source directory")
+    databasePtr := fs.String("database", "", "database connection url")
+    fs.Parse(args)
+
+    fmt.Println(*sourcePtr, *databasePtr)
+
+    migrater, err := internal.NewMigrater(*sourcePtr, *databasePtr)
+    if err != nil {
+        fmt.Println("error creating migrater")
+        return
+    }
+
+    migrater.RunMigrations(internal.DOWN)
 }
